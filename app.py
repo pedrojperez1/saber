bd766b24dcba52ffeee2dd1d7ec9f8089166e6d7
Pedro Perez <pedrojperez1@gmail.com>
2020-09-19 22:24:59 -0400

    change db name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///eyamei_db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///saber_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'oh-so-secret'
 
